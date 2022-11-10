1398fb96fc9f0760315919ef72484bef4592ba1a
beautiful28 <zingadanitza@gmail.com>
2022-11-10 16:00:17 +0300

    Mise en marche du chrone

--- src/components/BtnComponent.js
+++ src/components/BtnComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 
-function BtnComponent() {
+function BtnComponent(props) {
   return (
     <div>
-      <button className="stopwatch-btn-gre">Start</button>
+      <button className="stopwatch-btn stopwatch-btn-gre"
+      onClick={props.start}><b>Start</b></button>
     </div>
   );
 }
